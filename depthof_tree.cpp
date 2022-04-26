242318d8e69c63afbc204da72a401d6cb462d339
Yves <37640312+dp-wu@users.noreply.github.com>
2022-04-25 23:06:47 -0700

    Create depthof_tree.cpp

---
+++ depthof_tree.cpp
@@ -0,0 +1,84 @@
+/*
+max height (depth) of a tree
+https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?isFullScreen=true
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        int h_left, h_right;
+        int h_max = -1; // (from the question: ) Note -The Height of binary tree with single node is taken as zero.
+        
+        if (root != NULL) {
+            h_left = this->height(root->left);
+            h_right = this->height(root->right);
+            h_max = ((h_left > h_right)? h_left: h_right) + 1;
+        }
+        
+        return h_max;
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
