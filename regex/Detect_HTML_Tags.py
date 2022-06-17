ce2a0a2315feb12c539d4accb2413f5b90f6a8b9
Yves <37640312+dp-wu@users.noreply.github.com>
2022-06-16 20:03:09 -0700

    added \b for spaces, passed all the test cases

--- regex/Detect_HTML_Tags.py
+++ regex/Detect_HTML_Tags.py
@@ -1,11 +1,8 @@
 # https://www.hackerrank.com/challenges/detect-html-tags/problem?isFullScreen=true
-# In the question it mentioned: there may be 1 or more spaces before or after the tag name (i.e. <    tag >).
-# But as we can see that I didn't code for the spaces (and so far I don't know how to) but it still passed all the test cases.
-# So... hmmm. :/
 import re
 
 def find_tags(h):
-    pattern = r"(?<=<)[A-Za-z0-9]+[\/]?(?=.*>)"
+    pattern = r"(?<=<)\b[A-Za-z0-9]+[\/]?\b(?=.*>)"
     m = re.findall(pattern, h)
     return m
 
@@ -16,7 +13,7 @@ def main():
         h = input()
         r += find_tags(h)
         n -= 1
-    result = sorted(set(r)) # the result needed to be sorted alphabetically somehow :/
+    result = sorted(set(r))
     print(*result, sep=';')
 
 main()
