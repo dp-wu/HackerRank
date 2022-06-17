e489861a53b4a1a490b35658d8d354213f6f0519
Yves <37640312+dp-wu@users.noreply.github.com>
2022-06-17 00:32:24 -0700

    Create HackerRank_Tweets.py

---
+++ regex/HackerRank_Tweets.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/hackerrank-tweets/problem?isFullScreen=true
+import re
+
+
+def main():
+    pattern = r"((?i)hackerrank)"
+    counter = 0
+    n = int(input())
+    for i in range(n):
+        s = input()
+        result = re.findall(pattern, s)
+        counter += len(result)
+    print(counter)
+
+main()
