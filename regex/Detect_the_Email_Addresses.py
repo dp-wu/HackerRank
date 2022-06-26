e788fbe95bb6bd89b6606a450f6ce5eb3899702d
Yves <37640312+dp-wu@users.noreply.github.com>
2022-06-26 12:47:58 -0700

    Create Detect_the_Email_Addresses.py

---
+++ regex/Detect_the_Email_Addresses.py
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/detect-the-email-addresses/problem?isFullScreen=true
+import re
+
+
+def main():
+    n = int(input())
+    email = r'\b[\w\d\._]+\@[\w\d\._]+\b' # I couldn't get group work here for some reason ()@\1
+    result = set() # this way we don't need to remove the duplicates manually, python is awesome :D
+    
+    for i in range(n):
+        s = input()
+        found = re.findall(email, s)
+        for i in found:
+            result.add(i)
+    
+    print(*(sorted(result)), sep=";")
+main()
